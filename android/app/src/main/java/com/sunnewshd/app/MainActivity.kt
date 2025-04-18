8af3d0cd7f26d07c9618051a5f8b268320ed1bc1
husnain-native <husnainarifqazi123@gmail.com>
2025-04-18 22:02:54 +0500

    push

--- android/app/src/main/java/com/sunnewshd/app/MainActivity.kt
+++ android/app/src/main/java/com/sunnewshd/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.husnainarif1.SunNews
+package com.sunnewshd.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/sunnewshd/app/MainApplication.kt
+++ android/app/src/main/java/com/sunnewshd/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.husnainarif1.SunNews
+package com.sunnewshd.app
 
 import android.app.Application
 import android.content.res.Configuration
